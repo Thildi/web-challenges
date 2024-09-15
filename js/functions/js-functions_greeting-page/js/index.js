f6d4cbfc0ffb403c1ccb8981437b84a821e85960
Marthilda <marthilda.vollmer@hotmail.de>
2024-09-15 15:17:15 +0200

    Solution

--- js/functions/js-functions_greeting-page/js/index.js
+++ js/functions/js-functions_greeting-page/js/index.js
@@ -23,10 +23,31 @@ Change the contents of this page depending on the current day and time.
 const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
+  const hours = new Date().getHours(); /*0-23 h*/
+  if (hours >= 6 && hours <= 12) {
+    return "Good Morning";
+  } else if (hours >= 13 && hours <= 18) {
+    return "Good Afternoon";
+  } else if (hours >= 19 && hours <= 22) {
+    return "Good Evening";
+  } else {
+    return "Good Night";
+  }
+
   // Code here
 }
 
 function getDayColor() {
+  const weekday = new Date().getDay(); /*0-6 sonntag-samstag*/
+  if (weekday === 1) {
+    return "darkgray";
+  }
+  if (weekday === 2 || weekday === 3 || weekday === 4 || weekday === 5) {
+    return "lightblue";
+  } else {
+    return "hotpink";
+  }
+
   // Code here
 }
 
