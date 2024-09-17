fc4506af7979968f74cc0c9c0577d4e647741702
Marthilda <marthilda.vollmer@hotmail.de>
2024-09-17 11:36:50 +0200

    Solution

--- js/forms/js-forms_checkbox-input/js/index.js
+++ js/forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js ="success"');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,9 +17,22 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  if (!tosCheckbox.checked) {
+    showTosError();
+    return;
+  } else {
+    hideTosError();
+    successMessage.removeAttribute("hidden");
+    alert("Form submitted");
+  }
 
   // --^-- write your code here --^--
 
-  // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  tosCheckbox.addEventListener("change", () => {
+    if (tosCheckbox.checked) {
+      hideTosError();
+    } else {
+      showTosError();
+    }
+  });
 });
