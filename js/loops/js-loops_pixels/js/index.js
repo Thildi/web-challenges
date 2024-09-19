791efb5b432ae85986466ce817617dcca47b0f12
Marthilda <marthilda.vollmer@hotmail.de>
2024-09-19 15:04:19 +0200

    Solution

--- js/loops/js-loops_pixels/js/index.js
+++ js/loops/js-loops_pixels/js/index.js
@@ -1,5 +1,5 @@
-console.clear();
 const pixelValues = [
+  // Zeile 1
   [
     "#abcccc",
     "#9bb9b7",
@@ -12,6 +12,7 @@ const pixelValues = [
     "#92afb9",
     "#9bb9b7",
   ],
+  // Zeile 2
   [
     "#9bb9b7",
     "#91a8b9",
@@ -24,6 +25,7 @@ const pixelValues = [
     "#89b9a9",
     "#9bb9b7",
   ],
+  // Zeile 3
   [
     "#9bb9b7",
     "#9bb9b7",
@@ -36,6 +38,7 @@ const pixelValues = [
     "#9bb9b7",
     "#9bb9b7",
   ],
+  // Zeile 4
   [
     "#9bb9b7",
     "#9bb9b7",
@@ -48,6 +51,7 @@ const pixelValues = [
     "#9bb9b7",
     "#9bb9b7",
   ],
+  // Zeile 5
   [
     "#9bb9b7",
     "#ed4913",
@@ -60,6 +64,7 @@ const pixelValues = [
     "#9bb9b7",
     "#94abb9",
   ],
+  // Zeile 6
   [
     "#9bb9b7",
     "#ec590b",
@@ -72,6 +77,7 @@ const pixelValues = [
     "#9bb9b7",
     "#9bb9b7",
   ],
+  // Zeile 7
   [
     "#9bb9b7",
     "#9bb9b7",
@@ -84,6 +90,7 @@ const pixelValues = [
     "#9bb4c0",
     "#9bb9b7",
   ],
+  // Zeile 8
   [
     "#96a8b9",
     "#9bb9b7",
@@ -96,6 +103,7 @@ const pixelValues = [
     "#9bb9b7",
     "#9bb9b7",
   ],
+  // Zeile 9
   [
     "#9bb9b7",
     "#9bb9b7",
@@ -108,6 +116,7 @@ const pixelValues = [
     "#9bb9b7",
     "#9aacb9",
   ],
+  // Zeile 10
   [
     "#9bb9b7",
     "#87b0b6",
@@ -121,20 +130,31 @@ const pixelValues = [
     "#9bb9b7",
   ],
 ];
-
 const canvas = document.querySelector('[data-js="canvas"]');
 
-function createPixels() {
-  for (let i = 0; i < pixelValues.length; i++) {
-    for (let j = 0; j < pixelValues[i].length; j++) {
-      const pixelColor = pixelValues[i][j]; // Hole die Farbe für das Pixel
-      const pixel = document.createElement("div"); // Erstelle ein neues div-Element
-      pixel.className = "pixel"; // Setze die Klasse auf 'pixel'
-      pixel.style.backgroundColor = pixelColor; // Setze die Hintergrundfarbe des Pixels
-      canvas.appendChild(pixel); // Füge das Pixel dem Canvas hinzu
-    }
-  }
-}
+/*
+Use the color values in the array `pixelValues` to create "pixel" elements and append them to the `canvas` element.
+
+- use `for` loops to generate the pixel elements
+- pixels should be a `div` element with the class `pixel` and the background color set to respective color from the `pixelValues` array.
+- You can append all pixels directly to the canvas, no need for extra row grouping etc.
+- Hint: The colors are stored in Arrays inside another Array. How can you access these values with for loops?
+
+Switch to the `./js/index.js` file and make something great happen! */
 
 // Rufe die Funktion auf, um die Pixel zu erstellen
 createPixels();
+
+function createPixels() {
+  // Schleife über jede Zeile im pixelValues Array:
+  for (let row = 0; row < pixelValues.length; row++) {
+    // Schleife über jede Spalte in der aktuellen Zeile des Arrays:
+    for (let column = 0; column < pixelValues[row].length; column++) {
+      const pixelColor = pixelValues[row][column];
+      const pixel = document.createElement("div");
+      pixel.className = "pixel";
+      pixel.style.backgroundColor = pixelColor;
+      canvas.appendChild(pixel);
+    }
+  }
+}
