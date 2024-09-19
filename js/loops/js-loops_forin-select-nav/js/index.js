00331ead264bca0f8da825dce57ea83317063158
Marthilda <marthilda.vollmer@hotmail.de>
2024-09-19 11:13:40 +0200

    Solution

--- js/loops/js-loops_forin-select-nav/js/index.js
+++ js/loops/js-loops_forin-select-nav/js/index.js
@@ -1,5 +1,25 @@
 console.clear();
 
+/*
+
+### Part 2: Creating a Navigation Bar
+
+Before we create a navigation bar, let's have a look what's already there in `./js/index.js` :
+
+- There is a `nav` object with the keys "home", "about", and "contact".
+- Each value is an object containing an "href" and "text" key and a string value.
+- A `nav` and `ul` element is created and appended to the `main` element.
+
+Your task is to fill this list with list items and anchor tags; as content, use the `nav` object which you can access with a `for...in` loop.
+
+Use the following hints as a guideline:
+
+- Write a `for...in` loop for the `nav` object.
+- In each iteration,
+  - create a list item and an anchor tag
+  - set the anchor tag's `href` and `textContent` properties to the relevant values of the `nav` object; you want to use bracket notation with your iterator variable
+  - append your created items accordingly.*/
+
 const main = document.querySelector('[data-js="main"]');
 
 // Part 1: Creating a Select Input
@@ -21,7 +41,12 @@ select.name = "languages";
 main.append(select);
 
 // --v-- write/change code here --v--
-
+for (const key in languages) {
+  const option = document.createElement("option");
+  option.value = key;
+  option.textContent = languages[key];
+  select.append(option);
+}
 // --^-- write/change code here --^--
 
 // Part 2: Creating a Navigation Bar
@@ -38,5 +63,13 @@ main.append(navElement);
 navElement.append(ul);
 
 // --v-- write/change code here --v--
+for (const key in nav) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = nav[key].href;
+  a.textContent = nav[key].text;
 
+  li.append(a);
+  ul.append(li);
+}
 // --^-- write/change code here --^--
