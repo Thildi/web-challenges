ed371e49eb17eb2c0b2025d6cac9a8b370989d63
Marthilda <marthilda.vollmer@hotmail.de>
2024-09-19 11:14:55 +0200

    Solution

--- js/loops/js-loops_forin-select-nav/js/index.js
+++ js/loops/js-loops_forin-select-nav/js/index.js
@@ -1,25 +1,5 @@
 console.clear();
 
-/*
-
-### Part 2: Creating a Navigation Bar
-
-Before we create a navigation bar, let's have a look what's already there in `./js/index.js` :
-
-- There is a `nav` object with the keys "home", "about", and "contact".
-- Each value is an object containing an "href" and "text" key and a string value.
-- A `nav` and `ul` element is created and appended to the `main` element.
-
-Your task is to fill this list with list items and anchor tags; as content, use the `nav` object which you can access with a `for...in` loop.
-
-Use the following hints as a guideline:
-
-- Write a `for...in` loop for the `nav` object.
-- In each iteration,
-  - create a list item and an anchor tag
-  - set the anchor tag's `href` and `textContent` properties to the relevant values of the `nav` object; you want to use bracket notation with your iterator variable
-  - append your created items accordingly.*/
-
 const main = document.querySelector('[data-js="main"]');
 
 // Part 1: Creating a Select Input
