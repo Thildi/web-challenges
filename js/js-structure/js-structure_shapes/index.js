c9aacbf857b3f0a1fd210da161fb599e6ee6b271
Marthilda <marthilda.vollmer@hotmail.de>
2024-09-20 15:17:53 +0200

    Solution

--- js/js-structure/js-structure_shapes/index.js
+++ js/js-structure/js-structure_shapes/index.js
@@ -1,3 +1,12 @@
+/*Open the [index.js](./index.js). You find 3 elements that are generated: A circle, a square and a pentagon. If one of the elements is clicked the color changes to gray. Let's use the `getRandomColor` function to make the elements more colorful!
+
+- import the function `getRandomColor` from the [utils/randomColor.js](./utils/randomColor.js) file.
+- use the function inside all three event listeners and apply the returned color to the backgroundColor.
+
+> 💡 Hint: if it doesn't work you might need to change something inside the `index.html` regarding the script tag ;)*/
+
+import getRandomColor from "./utils/randomColor.js";
+
 console.clear();
 
 const root = document.getElementById("root");
@@ -5,19 +14,22 @@ const root = document.getElementById("root");
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
+  const randomColor = getRandomColor();
+  circle.style.backgroundColor = randomColor;
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  const randomColor = getRandomColor();
+  square.style.backgroundColor = randomColor;
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  const randomColor = getRandomColor();
+  pentagon.style.backgroundColor = randomColor;
 });
 
 root.append(circle, square, pentagon);
