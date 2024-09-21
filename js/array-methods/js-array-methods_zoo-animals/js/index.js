2c2716aee5e73ecbfd2063c00cbabcc58be64747
Marthilda <marthilda.vollmer@hotmail.de>
2024-09-21 13:31:52 +0200

    Solution

--- js/array-methods/js-array-methods_zoo-animals/js/index.js
+++ js/array-methods/js-array-methods_zoo-animals/js/index.js
@@ -5,7 +5,8 @@ import { zooAnimals } from "../utils/db.js";
 // The function should return 'true' if the animal name is included in the array or 'false' if not.
 
 function hasAnimal(animals, animalName) {
-  return null;
+  const lowercasedAnimals = animals.map((animal) => animal.toLowerCase());
+  return lowercasedAnimals.includes(animalName.toLowerCase());
 }
 
 // Bonus:
