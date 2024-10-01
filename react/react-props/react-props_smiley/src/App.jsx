0445b45938ebedd0ad675f464b3db4f80243197c
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-01 12:20:16 +0200

    Add comments

--- react/react-props/react-props_smiley/src/App.jsx
+++ react/react-props/react-props_smiley/src/App.jsx
@@ -8,3 +8,14 @@ export default function App() {
 function Smiley({ isHappy }) {
   return <h1>{isHappy === true ? "😁" : "😩"}</h1>;
 }
+
+/* Es ist zwar schön, das so direkt im JSX zu schrieben, bei komplexeren Anwendungen sollte
+man aber Variablen deklarieren für bessere Lesbarkeit, Wiederverwendbarkeit, bessere Fehlerbehandlung
+Trennung von Logik und Darstellung und zu Debugging-Zwecken mit concole.log() 
+
+Hier der alte Code, bei dem Logik und Darstellung getrennt waren:
+function Smiley({ isHappy }) {
+  const displayedSmiley = isHappy === true ? "😁" : "😩";
+  return <h1>{displayedSmiley}</h1>;
+}
+*/
