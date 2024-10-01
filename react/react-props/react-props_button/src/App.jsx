a4779e21d691597f2174568673498306120a9248
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-01 12:01:32 +0200

    Solution

--- react/react-props/react-props_button/src/App.jsx
+++ react/react-props/react-props_button/src/App.jsx
@@ -2,5 +2,28 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("You clicked the button!");
+  }
+  return (
+    <Button
+      text="Click me"
+      backgroundColor="lightgreen"
+      color="blue"
+      disabled={false}
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button({ color, backgroundColor, disabled, text, onClick }) {
+  return (
+    <button
+      style={{ color: color, backgroundColor: backgroundColor }}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
