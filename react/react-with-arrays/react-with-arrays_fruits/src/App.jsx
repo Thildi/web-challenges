ca65a845882a1fe81e65f246b005dfc820402582
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-02 15:02:27 +0200

    Solution

--- react/react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react/react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,39 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍒 Cherry",
+      color: "red",
+    },
+    {
+      id: 1339,
+      name: "🍓 Strawberry",
+      color: "red",
+    },
+    {
+      id: 1331,
+      name: "🍍 Pineapple",
+      color: "yellow",
+    },
+    {
+      id: 1332,
+      name: "🍏 Apple",
+      color: "green",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} />
+      ))}
     </div>
   );
 }
