210ced6b8b591c499584741ed1b655bdd1600977
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-04 11:26:55 +0200

    Solution

--- react/react-state/react-state-2_favourite-holiday/src/App.jsx
+++ react/react-state/react-state-2_favourite-holiday/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
+    const valueHoliday = event.target.holiday.value;
+    const valueDate = event.target.date.value;
+
+    setHoliday(valueHoliday);
+    setDate(valueDate);
+
+    event.target.reset();
+    document.getElementById("holiday").focus();
   }
 
   return (
@@ -30,10 +42,10 @@ export default function App() {
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
