e7d366597f3672d253a3873d591d613224d0f35c
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-07 11:41:31 +0200

    Solution

--- react/react-state/react-state-3_adding-animals/src/App.jsx
+++ react/react-state/react-state-3_adding-animals/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form";
 import List from "./components/List";
+import { uid } from "uid";
 
 const initialAnimals = [
   {
@@ -16,8 +17,8 @@ const initialAnimals = [
   },
   {
     id: "Qjf_K7V",
-    name: "Beaver",
-    emoji: "🦫",
+    name: "Spider",
+    emoji: "🕷",
   },
 ];
 
@@ -26,6 +27,7 @@ export default function App() {
 
   function handleAddAnimal(newAnimal) {
     console.log(newAnimal);
+    setAnimals([...animals, { id: uid(), ...newAnimal }]);
   }
 
   return (
