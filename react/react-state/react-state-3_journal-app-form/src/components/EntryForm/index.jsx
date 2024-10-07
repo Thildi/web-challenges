bc57cecab9168f1950c46e9b0286e34f770c2db7
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-07 12:33:17 +0200

    Solution

--- react/react-state/react-state-3_journal-app-form/src/components/EntryForm/index.jsx
+++ react/react-state/react-state-3_journal-app-form/src/components/EntryForm/index.jsx
@@ -1,9 +1,22 @@
 import "./EntryForm.css";
 import Button from "../Button";
 
-export default function EntryForm() {
+export default function EntryForm({ onAddEntry }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const newEntry = {
+      motto: formData.get("motto"),
+      notes: formData.get("notes"),
+    };
+
+    onAddEntry(newEntry);
+    event.target.reset();
+  }
+
   return (
-    <form className="entry-form">
+    <form className="entry-form" onSubmit={handleSubmit}>
       <h2 className="entry-form__title">New Entry</h2>
       <div className="entry-form__fields">
         <div className="entry-form__field">
