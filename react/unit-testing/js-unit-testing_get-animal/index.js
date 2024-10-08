3ac7de7a0d000149cfe3038d20c2e5343ac9fc13
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-08 11:52:17 +0200

    Solution

--- react/unit-testing/js-unit-testing_get-animal/index.js
+++ react/unit-testing/js-unit-testing_get-animal/index.js
@@ -1,9 +1,9 @@
 export function getAnimal(animal) {
   if (animal === "cats") {
     return "I totally love cats!";
-  } else if (animal === "dogs") {
-    return "I like dogs!";
-  } else {
+  } else if (animal === undefined) {
     return "I do not like animals at all!";
+  } else {
+    return `I like ${animal}!`;
   }
 }
