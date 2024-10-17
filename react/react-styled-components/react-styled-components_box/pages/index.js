a8fe76332020e6db22e3a86eb31b4f0127661e6c
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-17 14:45:25 +0200

    Solution

--- react/react-styled-components/react-styled-components_box/pages/index.js
+++ react/react-styled-components/react-styled-components_box/pages/index.js
@@ -1,10 +1,17 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
-
+import BoxWithStyledComponents from "../components/BoxWithClassName/BoxWithStyledComponents.js";
+import styled from "styled-components";
 export default function HomePage() {
   return (
-    <div>
+    <FlexContainer $column>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents $isBlack />
+    </FlexContainer>
   );
 }
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: ${(props) => (props.$column ? "column" : "row")};
+`;
